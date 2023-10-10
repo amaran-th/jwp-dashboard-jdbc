cb638595ac90f23e55974d9b4ee0a23371d837e0
amaran-th <songsy405@naver.com>
2023-10-10 14:55:33 +0900

    feat: AOP 학습테스트 1단계 진행

--- study/src/test/java/aop/stage0/TransactionHandler.java
+++ study/src/test/java/aop/stage0/TransactionHandler.java
@@ -1,5 +1,11 @@
 package aop.stage0;
 
+import aop.DataAccessException;
+import aop.Transactional;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
@@ -8,8 +14,33 @@ public class TransactionHandler implements InvocationHandler {
     /**
      * @Transactional 어노테이션이 존재하는 메서드만 트랜잭션 기능을 적용하도록 만들어보자.
      */
+
+    private final PlatformTransactionManager platformTransactionManager;
+    private final Object target;
+
+    public TransactionHandler(final PlatformTransactionManager platformTransactionManager, final Object target) {
+        this.platformTransactionManager = platformTransactionManager;
+        this.target = target;
+    }
+
     @Override
     public Object invoke(final Object proxy, final Method method, final Object[] args) throws Throwable {
-        return null;
+        final Method targetMethod = target.getClass().getDeclaredMethod(method.getName(), method.getParameterTypes());
+        if (targetMethod.isAnnotationPresent(Transactional.class)) {
+            return doTransaction(targetMethod, args);
+        }
+        return targetMethod.invoke(target, args);
+    }
+
+    private Object doTransaction(Method method, Object[] args) {
+        TransactionStatus status = platformTransactionManager.getTransaction(new DefaultTransactionDefinition());
+        try {
+            final Object result = method.invoke(target, args);
+            platformTransactionManager.commit(status);
+            return result;
+        } catch (Exception e) {
+            platformTransactionManager.rollback(status);
+            throw new DataAccessException(e);
+        }
     }
 }
