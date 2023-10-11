31530c17d873495105e9ed0372984c4056c006f4
amaran-th <songsy405@naver.com>
2023-10-11 21:10:07 +0900

    feat: AOP 학습테스트 3단계 진행

--- study/src/test/java/aop/stage2/AopConfig.java
+++ study/src/test/java/aop/stage2/AopConfig.java
@@ -1,8 +1,22 @@
 package aop.stage2;
 
+import aop.stage1.TransactionAdvice;
+import aop.stage1.TransactionAdvisor;
+import aop.stage1.TransactionPointcut;
+import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 public class AopConfig {
+    @Bean
+    TransactionAdvisor transactionAdvisor(final PlatformTransactionManager platformTransactionManager) {
+        return new TransactionAdvisor(new TransactionPointcut(), new TransactionAdvice(platformTransactionManager));
+    }
 
+    @Bean
+    DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator() {
+        return new DefaultAdvisorAutoProxyCreator();
+    }
 }
